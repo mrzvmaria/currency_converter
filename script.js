a0da9e0be637eb1b7507952ec4ac4809ba1bd7dd
Maria <mrzvmaria@yandex.ru>
2020-11-01 11:56:19 +0300

    added to GitHub

---
+++ script.js
@@ -0,0 +1,27 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+  let request = new XMLHttpRequest();
+
+  // request.open(method, url, async, login, pass);
+  request.open('GET', 'current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
+
+  //status - статус сервера (404 not found)
+  //statusText - текстовое сообщение от сервера
+  //responseText - текст ответа сервера (от бэкэдн-разработчика)
+  //readyState - текущее состояние запроса
+
+  request.addEventListener('readystatechange', function() {
+    if (request.readyState === 4 && request.status == 200) {
+      let data = JSON.parse(request.response);
+
+      inputUsd.value = inputRub.value / data.usd;
+    } else {
+      inputUsd.value = "Что-то пошло не так!";
+    }
+  });
+
+})
